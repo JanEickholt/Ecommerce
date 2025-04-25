164f7083761d9ad2f5f8c018f2e32ed81cd242c9
Jan Eickholt <eickholtjan@gmail.com>
2025-04-25 17:14:04 +0200

    ADD: some templating files

---
+++ src/app/features/home/components/categories-showcase/categories-showcase.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-showcase',
+  standalone: true,
+  imports: [],
+  templateUrl: './categories-showcase.component.html',
+  styleUrl: './categories-showcase.component.scss'
+})
+export class CategoriesShowcaseComponent {
+
+}
